ca6c849910b00bd4f4d06d1bac1a9d32aeae71a9
David Perez <david@closemarketing.es>
2015-01-17 20:22:11 +0100

    Arreglados errores Non-static method GFVtiger::get_base_url() should not be called statically

--- gravityforms-vtiger.php
+++ gravityforms-vtiger.php
@@ -1130,12 +1130,12 @@ class GFVtiger {
     }
 
     //Returns the url of the plugin's root folder
-    protected function get_base_url(){
+    protected static function get_base_url(){
         return plugins_url(null, __FILE__);
     }
 
     //Returns the physical path of the plugin's root folder
-    protected function get_base_path(){
+    protected static function get_base_path(){
         $folder = basename(dirname(__FILE__));
         return WP_PLUGIN_DIR . "/" . $folder;
     }
