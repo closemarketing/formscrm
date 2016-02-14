5b2e276e6eea003162365b0b1562f25df117f4cd
David Perez <david@closemarketing.es>
2016-02-14 23:21:41 +0100

    added Solve360

--- class-gf-crm.php
+++ class-gf-crm.php
@@ -107,6 +107,10 @@ class GFCRM extends GFFeedAddOn {
                                                     array(
                                                         'label' => 'Bitrix24',
                                                         'name'  => 'bitrix24'
+                                                    ),
+                                                    array(
+                                                        'label' => 'Solve360',
+                                                        'name'  => 'solve360'
                                                     )
                                                 )
 					),
@@ -143,7 +147,7 @@ class GFCRM extends GFFeedAddOn {
 						//'feedback_callback' => $this->login_api_crm(),
                         'tooltip'       => __( 'Find the API Password in the profile of the user in CRM.', 'gravityformscrm' ),
                         'tooltip_class'     => 'tooltipclass',
-                        'dependency' => array( 'field' => 'gf_crm_type', 'values' => array( 'vTiger','VTE CRM' ) ),
+                        'dependency' => array( 'field' => 'gf_crm_type', 'values' => array( 'vTiger','VTE CRM','Solve360' ) ),
 					),
 					array(
 						'name'  => 'gf_crm_apisales',
@@ -193,7 +197,7 @@ class GFCRM extends GFFeedAddOn {
 		// ensures valid credentials were entered in the settings page
 		if ( $this->login_api_crm() == false ) {
 			?>
-			<div><?php echo sprintf( __( 'We are unable to login to CRM with the provided API key or URL is incorrect (it must finish with slash / ). Please make sure you have entered a valid API key in the %sSettings Page%s', 'gravityformscrm' ),
+			<div><?php echo sprintf( __( 'We are unable to login to CRM.', 'gravityformscrm' ),
 					'<a href="' . $this->get_plugin_settings_url() . '">', '</a>' ); ?>
 			</div>
 			<?php
@@ -285,6 +289,9 @@ class GFCRM extends GFFeedAddOn {
 
          } elseif($crm_type == 'Bitrix24') {
              $custom_fields = $this->bitrix_listfields($username, $password, $url, 'Leads');
+
+         } elseif($crm_type == 'Solve360') {
+             $custom_fields = $this->solve360_listfields($username, $apipassword, 'contacts');
 
         } // From if CRM
 
@@ -411,6 +418,8 @@ class GFCRM extends GFFeedAddOn {
 			$crmport ="443";
 			$id = $this->bitrix_create_lead($username, $password, $url, $crmport, "Leads", $merge_vars);
 
+        } elseif($crm_type == 'Solve360') {
+            $id = $this->solve360_createcontact($username, $apipassword, 'contacts', $merge_vars);
         } // From CRM IF
 
         //Sends email if it does not create a lead
@@ -518,6 +527,8 @@ class GFCRM extends GFFeedAddOn {
 		$crmport ="443"; //Assumed by default
         $login_result = $this-> bitrix_login($username, $password, $url, $crmport);
 
+    } elseif($crm_type == 'Solve360') {
+        $login_result = $this-> solve360_login($username, $apipassword);
     } //OF CRM
 
     $this->debugcrm($login_result);
@@ -1838,6 +1849,114 @@ private function bitrix_create_lead($username, $password, $url, $crmport, $modul
 	}
 }
 ///////////////// Bitrix 24 CRM ////////////////////////////////
+
+///////////////// Solve360 CRM ////////////////////////////////
+private function solve360_login($username, $password){
+	$url = 'https://secure.solve360.com/contacts?limit=1';
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml; charset=UTF-8'));
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_setopt($ch, CURLOPT_USERPWD, $username.":".$password);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	$doc = new DomDocument();
+	$doc->loadXML($data);
+
+	$this->debugcrm($doc);
+	if(isset($doc->getElementsByTagName("errors")->item(0)->nodeValue))
+		$errorDetails = $doc->getElementsByTagName("errors")->item(0)->nodeValue;
+	else
+		$errorDetails = '';
+
+	if(!empty($errorDetails)){
+		echo '<div id="message" class="error below-h2">
+				<p><strong>'.$errorDetails.': </strong></p></div>';
+		return FALSE;
+	}
+	else{
+		return TRUE;
+	}
+}
+private function solve360_listfields($username, $password, $module) {
+	$url = 'https://secure.solve360.com/'.$module."/fields";
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/xml; charset=UTF-8'));
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_setopt($ch, CURLOPT_USERPWD, $username.":".$password);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	$this->debugcrm($data);
+	if ($data) {
+		$xml = simplexml_load_string($data);
+		$json_string = json_encode($xml);
+		$result_array = json_decode($json_string, TRUE);
+		if (isset($result_array['errors'])) {
+			echo '<div id="message" class="error below-h2">';
+			echo 'Error while retriving fields'.'<br/>Error: '.$result_array['errors'];
+			echo '</div>';
+		}
+		else{
+			foreach ($xml->fields->field as $element) {
+				$fields[]=array(
+					'label' => (string)$element->label,
+					'name' =>  (string)$element->name,
+					'required' => FALSE
+				);
+			}
+		}
+	} else {
+		// Something went wrong and we haven't got xml in the rsolvense
+		throw new Exception('System error while working with Solve360 service');
+	}
+	return $fields;
+}
+private function solve360_createcontact($username, $password, $module, $merge_vars){
+	$url = 'https://secure.solve360.com/'.$module;
+	$vars = array();
+	foreach($merge_vars as $var){
+		$vars[$var['name']] =  $var['value'];
+	}
+	$data_string = json_encode($vars);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'Content-Type: application/json',
+		'Content-Length: ' .strlen($data_string))
+			   );
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_setopt($ch, CURLOPT_USERPWD, $username.":".$password);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	//execute post
+	$result = curl_exec($ch);
+	//close connection
+	curl_close($ch);
+
+	if ($result) {
+		$xml = simplexml_load_string($result);
+		$json_string = json_encode($xml);
+		$result_array = json_decode($json_string, TRUE);
+		if (isset($result_array['errors'])) {
+			echo 'Error while creating record'.'<br/>Error: '.$result_array['errors'];
+		}
+		else{
+			 return $result_array['item']['id'];
+		}
+	} else {
+		// Something went wrong and we haven't got xml in the rsolvense
+		throw new Exception('System error while working with Solve360 service');
+	}
+	return NULL;
+}
+///////////////// Solve360 CRM ////////////////////////////////
 
     ////////////////////////////////
 
