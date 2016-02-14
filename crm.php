3abc989f16db5716dc690157ef67f5ccf6c4506e
David Perez <david@closemarketing.es>
2016-02-14 23:21:28 +0100

    change version 2.1

--- crm.php
+++ crm.php
@@ -3,7 +3,7 @@
 Plugin Name: Gravity Forms CRM Add-On
 Plugin URI: http://codecanyon.net/item/gravity-forms-crm-addon/10521695
 Description: Integrates Gravity Forms with CRM allowing form submissions to be automatically sent to your CRM.
-Version: 2.0
+Version: 2.1
 Author: closemarketing
 Author URI: http://www.closemarketing.es
 
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
 
-define( 'GF_CRM_VERSION', '2.0' );
+define( 'GF_CRM_VERSION', '2.1' );
 
 add_action( 'gform_loaded', array( 'GF_CRM_Bootstrap', 'load' ), 5 );
 
@@ -47,6 +47,3 @@ class GF_CRM_Bootstrap {
 function gf_crm(){
 	return GFCRM::get_instance();
 }
-//Updates Plugin
-require_once('wp-updates-plugin.php');
-new WPUpdatesPluginUpdater_905( 'http://wp-updates.com/api/2/plugin', plugin_basename(__FILE__));
