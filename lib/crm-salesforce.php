cf0b7328989bda79a9984fcaef610497b170668f
David Perez <david@closemarketing.es>
2016-06-30 13:09:26 +0200

    solved library salesforce

--- lib/crm-salesforce.php
+++ lib/crm-salesforce.php
@@ -3,18 +3,17 @@
 /////// SALESFORCE CRM ///////
 //////////////////////////////
 
-private function salesforce_login($username, $password) {
-    require_once ('lib/salesforce/SforcePartnerClient.php');
-    require_once ('lib/salesforce/SforceHeaderOptions.php');
+function salesforce_login($username, $password) {
+    require_once ('salesforce/SforcePartnerClient.php');
+    require_once ('salesforce/SforceHeaderOptions.php');
 
     //Return true or false for logged in
 	try {
 			$mySforceConnection = new SforcePartnerClient();
-			$mySoapClient = $mySforceConnection->createConnection(plugin_dir_path( __FILE__ ).'lib/salesforce/partner.wsdl.xml');
+			$mySoapClient = $mySforceConnection->createConnection(plugin_dir_path( __FILE__ ).'salesforce/partner.wsdl.xml');
 			$mylogin = $mySforceConnection->login($username, $password);
 
-            $this->debugcrm($mylogin->userInfo);
-			return true;
+			return $mylogin->userInfo->userId;
 		}
 		catch (Exception $e)
 		{
@@ -25,22 +24,21 @@ private function salesforce_login($username, $password) {
 		return false;
 }
 
-private function salesforce_listfields($username, $password, $module) {
-    require_once ('lib/salesforce/SforcePartnerClient.php');
-    require_once ('lib/salesforce/SforceHeaderOptions.php');
+function salesforce_listfields($username, $password, $module) {
+    require_once ('salesforce/SforcePartnerClient.php');
+    require_once ('salesforce/SforceHeaderOptions.php');
 
 	try {
 			$mySforceConnection = new SforcePartnerClient();
-			$mySoapClient = $mySforceConnection->createConnection(plugin_dir_path( __FILE__ ).'lib/salesforce/partner.wsdl.xml');
+			$mySoapClient = $mySforceConnection->createConnection(plugin_dir_path( __FILE__ ).'salesforce/partner.wsdl.xml');
 			$mylogin = $mySforceConnection->login($username, $password);
 			$myobj= $mySforceConnection->describeSObject($module);
-			//return $myobj->fields;
-            $this->debugcrm($myobj->fields);
+
 			$entityArray = array();
 			foreach($myobj->fields as $field){
 				$entityArray[]=array('label'=> $field->label, 'name' => $field->name, 'required' => !($field->nillable==1)&&($field->defaultedOnCreate!=1)&&($field->name!='Name') );
 			}
-
+            echo '<div id="message" class="updated below-h2"><p><strong>'.__('Logged correctly in', 'gravityformscrm').' Salesforce</strong></p></div>';
 			return $entityArray;
 
 		}
@@ -51,13 +49,13 @@ private function salesforce_listfields($username, $password, $module) {
 	}
 }
 
-private function salesforce_create_lead($username, $password, $module, $mergevars) {
-    require_once ('lib/salesforce/SforcePartnerClient.php');
-    require_once ('lib/salesforce/SforceHeaderOptions.php');
+function salesforce_create_lead($username, $password, $module, $mergevars) {
+    require_once ('salesforce/SforcePartnerClient.php');
+    require_once ('salesforce/SforceHeaderOptions.php');
 
 	try {
 			$mySforceConnection = new SforcePartnerClient();
-			$mySoapClient = $mySforceConnection->createConnection(plugin_dir_path( __FILE__ ).'lib/salesforce/partner.wsdl.xml');
+			$mySoapClient = $mySforceConnection->createConnection(plugin_dir_path( __FILE__ ).'salesforce/partner.wsdl.xml');
 			$mylogin = $mySforceConnection->login($username, $password);
 
 			$fieldsArray = array();
@@ -71,7 +69,7 @@ private function salesforce_create_lead($username, $password, $module, $mergevar
 
 			$createResponse = $mySforceConnection->create(array($sObject));
 
-            $this->debugcrm($createResponse);
+            if (WP_DEBUG==true) { print_r($createResponse); }
 
 			return $createResponse[0]->id;
 		}
