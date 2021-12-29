a402576f41b14f2fc3648664a650462d7e250c73
David Perez <david@closemarketing.es>
2021-12-29 10:19:31 +0100

    better error management

--- includes/class-crmlib-clientify.php
+++ includes/class-crmlib-clientify.php
@@ -29,14 +29,27 @@ class CRMLIB_Clientify {
 		);
 		$url      = 'https://api.clientify.net/v1/' . $url;
 		$response = wp_remote_get( $url, $args );
+		$code   = isset( $result['response']['code'] ) ? (int) round( $result['response']['code'] / 100, 0 ) : 0;
 
-		if ( is_wp_error( $response ) ) {
-			formscrm_error_admin_message( 'ERROR', $response->errors['http_request_failed'][0] );
-			return false;
+		if ( 2 !== $code ) {
+			$message = implode( ' ', $result['response'] ) . ' ';
+			$body    = json_decode( $result['body'], true );
+			if ( is_array( $body ) ) {
+				foreach ( $body as $key => $value ) {
+					$message .= $key . ': ' . implode( '.', $value );
+				}
+			}
+			formscrm_error_admin_message( 'ERROR', $message );
+			return array(
+				'status' => 'error',
+				'data'   => $message,
+			);
 		} else {
-			$body = wp_remote_retrieve_body( $response );
-
-			return json_decode( $body, true );
+			$body = wp_remote_retrieve_body( $result );
+			return array(
+				'status' => 'ok',
+				'data'   => json_decode( $body, true ),
+			);
 		}
 	}
 	/**
@@ -54,14 +67,28 @@ class CRMLIB_Clientify {
 			'timeout' => 120,
 			'body'    => wp_json_encode( $bodypost ),
 		);
-		$response = wp_remote_post( 'https://api.clientify.net/v1/' . $url, $args );
-		if ( is_wp_error( $response ) ) {
-			formscrm_error_admin_message( 'ERROR', $response->errors['http_request_failed'][0] );
-			return false;
-		} else {
-			$body = wp_remote_retrieve_body( $response );
+		$result = wp_remote_post( 'https://api.clientify.net/v1/' . $url, $args );
+		$code   = isset( $result['response']['code'] ) ? (int) round( $result['response']['code'] / 100, 0 ) : 0;
 
-			return json_decode( $body, true );
+		if ( 2 !== $code ) {
+			$message = implode( ' ', $result['response'] ) . ' ';
+			$body    = json_decode( $result['body'], true );
+			if ( is_array( $body ) ) {
+				foreach ( $body as $key => $value ) {
+					$message .= $key . ': ' . implode( '.', $value );
+				}
+			}
+			formscrm_error_admin_message( 'ERROR', $message );
+			return array(
+				'status' => 'error',
+				'data'   => $message,
+			);
+		} else {
+			$body = wp_remote_retrieve_body( $result );
+			return array(
+				'status' => 'ok',
+				'data'   => json_decode( $body, true ),
+			);
 		}
 	}
 
@@ -116,69 +143,69 @@ class CRMLIB_Clientify {
 		$fields = array();
 		if ( 'contacts' === $module ) {
 			$fields[] = array( 'name' => 'owner', 'label' => __( 'username of the owner of the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'first_name', 'label' => __( 'contact first name', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'last_name', 'label' => __( 'Contact last name', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'phone', 'label' => __( 'Phone', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'company', 'label' => __( 'Company name', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'email', 'label' => __( 'Email', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'website', 'label' => __( 'Website', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'status', 'label' => __( 'Stores the contact status identifier', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'picture_url', 'label' => __( 'url of the picture for the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'title', 'label' => __( 'Contact title', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'summary', 'label' => __( 'Summary', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'description', 'label' => __( 'Description', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'remarks', 'label' => __( 'Remarks', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'message', 'label' => __( 'Message text to be shown in the contact wall', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'lead_scoring', 'label' => __( 'Lead scoring points', 'formscrm' ), 'required' => false , );
+			$fields[] = array( 'name' => 'first_name', 'label' => __( 'contact first name', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'last_name', 'label' => __( 'Contact last name', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'phone', 'label' => __( 'Phone', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'company', 'label' => __( 'Company name', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'email', 'label' => __( 'Email', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'website', 'label' => __( 'Website', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'status', 'label' => __( 'Stores the contact status identifier', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'picture_url', 'label' => __( 'url of the picture for the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'title', 'label' => __( 'Contact title', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'summary', 'label' => __( 'Summary', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'description', 'label' => __( 'Description', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'remarks', 'label' => __( 'Remarks', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'message', 'label' => __( 'Message text to be shown in the contact wall', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'lead_scoring', 'label' => __( 'Lead scoring points', 'formscrm' ), 'required' => false, );
 
-			$fields[] = array( 'name' => 'taxpayer_identification_number', 'label' => __( 'Taxpayer identification nummber', 'formscrm' ), 'required' => false , );
+			$fields[] = array( 'name' => 'taxpayer_identification_number', 'label' => __( 'Taxpayer identification nummber', 'formscrm' ), 'required' => false, );
 			$fields[] = array( 'name' => 'tags', 'label' => __( 'Array of strings with the tags of the contact (value separated by comma)', 'formscrm' ), 'required' => false, );
-			$fields[] = array( 'name' => 'gdpr_accept', 'label' => __( 'True if the user accepted the GDPR false if not', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'contact_source', 'label' => __( 'Contact source', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'medium', 'label' => __( 'Contact Medium', 'formscrm' ), 'required' => false , );
+			$fields[] = array( 'name' => 'gdpr_accept', 'label' => __( 'True if the user accepted the GDPR false if not', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'contact_source', 'label' => __( 'Contact source', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'medium', 'label' => __( 'Contact Medium', 'formscrm' ), 'required' => false, );
 			$fields[] = array( 'name' => 'contact_type', 'label' => __( 'Contact type', 'formscrm' ), 'required' => false, );
 			$fields[] = array( 'name' => 'birthday', 'label' => __( 'Birthday date', 'formscrm' ), 'required' => false, );
-			$fields[] = array( 'name' => 'pinterest_url', 'label' => __( 'URL of the Pinterest site of the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'twitter_url', 'label' => __( 'URL of the twitter site for the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'facebook_url', 'label' => __( 'url of the facebook site for the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'linkedin_url', 'label' => __( 'URL of the Linkedin site for the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'googleplus_url', 'label' => __( 'URL of the Google Plus site for the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'foursquare_url', 'label' => __( 'Foursquare id', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'klout_url', 'label' => __( 'url of the klout picture for the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'skype_username', 'label' => __( 'Skype username for the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'twitter_id', 'label' => __( 'Id of the contact in twitter', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'google_id', 'label' => __( 'Google id', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'facebook_id', 'label' => __( 'Facebook id', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'linkedin_id', 'label' => __( 'Linkedin user id', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'facebook_picture_url', 'label' => __( 'url of the facebook picture for the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'twitter_picture_url', 'label' => __( 'url of the twitter picture for the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'linkedin_picture_url', 'label' => __( 'url of the Linkedin picture for the contact', 'formscrm' ), 'required' => false , );
+			$fields[] = array( 'name' => 'pinterest_url', 'label' => __( 'URL of the Pinterest site of the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'twitter_url', 'label' => __( 'URL of the twitter site for the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'facebook_url', 'label' => __( 'url of the facebook site for the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'linkedin_url', 'label' => __( 'URL of the Linkedin site for the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'googleplus_url', 'label' => __( 'URL of the Google Plus site for the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'foursquare_url', 'label' => __( 'Foursquare id', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'klout_url', 'label' => __( 'url of the klout picture for the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'skype_username', 'label' => __( 'Skype username for the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'twitter_id', 'label' => __( 'Id of the contact in twitter', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'google_id', 'label' => __( 'Google id', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'facebook_id', 'label' => __( 'Facebook id', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'linkedin_id', 'label' => __( 'Linkedin user id', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'facebook_picture_url', 'label' => __( 'url of the facebook picture for the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'twitter_picture_url', 'label' => __( 'url of the twitter picture for the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'linkedin_picture_url', 'label' => __( 'url of the Linkedin picture for the contact', 'formscrm' ), 'required' => false, );
 		} elseif ( 'companies' === $module ) {
-			$fields[] = array( 'name' => 'sector', 'label' => __( 'Sector', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'company_sector', 'label' => __( 'Sector of company', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'business_name', 'label' => __( 'Business Name', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'taxpayer_identification_number', 'label' => __( 'Taxpayer identification nummber', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'fax', 'label' => __( 'Fax', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'number_of_employees', 'label' => __( 'Number of employees', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'owner', 'label' => __( 'username of the owner of the contact', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'email', 'label' => __( 'Email of company', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'phone', 'label' => __( 'Phone of company', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'website', 'label' => __( 'Website', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'rank', 'label' => __( 'Rank', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'rank_manual', 'label' => __( 'Rank Manual', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'picture_url', 'label' => __( 'Picture URL', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'facebook_url', 'label' => __( 'Facebook URL', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'linkedin_url', 'label' => __( 'Linkedin URL', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'twitter_url', 'label' => __( 'Twitter URL', 'formscrm' ), 'required' =>  false, );
-			$fields[] = array( 'name' => 'facebook_id', 'label' => __( 'Facebook ID', 'formscrm' ), 'required' =>  false, );
-			$fields[] = array( 'name' => 'twitter_id', 'label' => __( 'Twitter ID', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'founded', 'label' => __( 'Founded', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'approx_employees', 'label' => __( 'Approximate employees', 'formscrm' ), 'required' =>  false, );
-			$fields[] = array( 'name' => 'description', 'label' => __( 'Description', 'formscrm' ), 'required' =>  false, );
-			$fields[] = array( 'name' => 'remarks', 'label' => __( 'Remarks', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'summary', 'label' => __( 'Summary', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'tags', 'label' => __( 'Tags', 'formscrm' ), 'required' => false , );
-			$fields[] = array( 'name' => 'linkedin_picture_url', 'label' => __( 'Linkedin Picture URL', 'formscrm' ), 'required' => false , );
+			$fields[] = array( 'name' => 'sector', 'label' => __( 'Sector', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'company_sector', 'label' => __( 'Sector of company', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'business_name', 'label' => __( 'Business Name', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'taxpayer_identification_number', 'label' => __( 'Taxpayer identification nummber', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'fax', 'label' => __( 'Fax', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'number_of_employees', 'label' => __( 'Number of employees', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'owner', 'label' => __( 'username of the owner of the contact', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'email', 'label' => __( 'Email of company', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'phone', 'label' => __( 'Phone of company', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'website', 'label' => __( 'Website', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'rank', 'label' => __( 'Rank', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'rank_manual', 'label' => __( 'Rank Manual', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'picture_url', 'label' => __( 'Picture URL', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'facebook_url', 'label' => __( 'Facebook URL', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'linkedin_url', 'label' => __( 'Linkedin URL', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'twitter_url', 'label' => __( 'Twitter URL', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'facebook_id', 'label' => __( 'Facebook ID', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'twitter_id', 'label' => __( 'Twitter ID', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'founded', 'label' => __( 'Founded', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'approx_employees', 'label' => __( 'Approximate employees', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'description', 'label' => __( 'Description', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'remarks', 'label' => __( 'Remarks', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'summary', 'label' => __( 'Summary', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'tags', 'label' => __( 'Tags', 'formscrm' ), 'required' => false, );
+			$fields[] = array( 'name' => 'linkedin_picture_url', 'label' => __( 'Linkedin Picture URL', 'formscrm' ), 'required' => false, );
 		}
 		return $fields;
 	}
@@ -196,11 +223,17 @@ class CRMLIB_Clientify {
 		$contact = array();
 
 		foreach ( $merge_vars as $element ) {
+			if ( is_array( $element['value'] ) ) {
+				$element['value'] = implode( ',', $element['value'] );
+			}
+
 			if ( strpos( $element['name'], '|' ) && 0 === strpos( $element['name'], 'custom_fields' ) ) {
 				$custom_field = explode( '|', $element['name'] );
 				$contact[ $custom_field[0] ][ $custom_field[1] ] = $element['value'];
 			} elseif ( 'tags' === $element['name'] && false != strpos( $element['value'], ',' ) ) {
 				$contact[ $element['name'] ] = explode( ',', $element['value'] );
+			} elseif ( 'tags' === $element['name'] && false === is_array( $element['value'] ) ) {
+				$contact[ $element['name'] ] = array( $element['value'] );
 			} else {
 				$contact[ $element['name'] ] = $element['value'];
 			}
@@ -212,14 +245,14 @@ class CRMLIB_Clientify {
 
 		$result = $this->post( $module, $contact, $apikey );
 
-		if ( isset( $result['id'] ) ) {
+		if ( 'ok' === $result['status'] ) {
 			$response_result = array(
 				'status'  => 'ok',
 				'message' => 'success',
-				'id'      => $result['id'],
+				'id'      => $result['data']['id'],
 			);
 		} else {
-			$message = isset( $result['detail'] ) ? $result['detail'] : '';
+			$message = isset( $result['data'] ) ? $result['data'] : '';
 			$response_result = array(
 				'status'  => 'error',
 				'message' => $message,
