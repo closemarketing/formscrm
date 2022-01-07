95fa7d135ba63826aa8590fbbef2d601e6467734
David Perez <david@closemarketing.es>
2022-01-07 09:08:46 +0100

    inmovilla added

--- includes/class-admin-options.php
+++ includes/class-admin-options.php
@@ -69,6 +69,7 @@ class FORMSCRM_Admin {
 				<li>Clientify</li>
 				<li>Odoo (Premium)</li>
 				<li>vTiger (Premium) <a href="https://checkout.freemius.com/mode/dialog/plugin/8767/plan/14673/licenses/1/currency/eur/" target="_blank"><?php esc_html_e( 'Buy', 'formscrm' ); ?></a></li>
+				<li>Inmovilla (Premium) <a href="https://close.technology/wordpress-plugins/formscrm-inmovilla/?utm_source=WordPress+Settings&utm_medium=plugin&utm_campaign=link" target="_blank"><?php esc_html_e( 'Buy', 'formscrm' ); ?></a></li>
 			</ul>
 			<a class="button button-primary" href="/wp-admin/admin.php?page=formscrm-addons"><?php esc_html_e( 'View all addons', 'formscrm' ); ?></a>
 		</div>
