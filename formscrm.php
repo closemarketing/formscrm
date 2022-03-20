6f535e36653a1c8001ee2ed4c66ab5d3e5874c54
David Perez <david@closemarketing.es>
2022-03-20 18:59:24 +0100

    beta 1

--- formscrm.php
+++ formscrm.php
@@ -3,7 +3,7 @@
  * Plugin Name: FormsCRM
  * Plugin URI:  https://closemarketing.net/formscrm
  * Description: Connects Forms with CRM.
- * Version:     3.7
+ * Version:     3.7-beta.1
  * Author:      Closemarketing
  * Author URI:  https://close.marketing
  * Text Domain: formscrm
