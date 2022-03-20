db54b3c3e7a169cbcba179922f2a40517a5b3dd7
David Perez <david@closemarketing.es>
2022-03-20 18:56:25 +0100

    silent debug

--- includes/debug.php
+++ includes/debug.php
@@ -11,23 +11,18 @@
  */
 
 if ( ! function_exists( 'formscrm_debug_message' ) ) {
+	/**
+	 * Debug message in log
+	 *
+	 * @param array $message Message.
+	 * @return void
+	 */
 	function formscrm_debug_message( $message ) {
-		if ( WP_DEBUG == true ) {
+		if ( true === WP_DEBUG ) {
 			if ( is_array( $message ) ) {
-				$message = $message['status'] . ' ' . $message['data'];
+				$message = print_r( $message, true ); //phpcs:ignore
 			}
-			// Debug Mode
-			echo '  <table class="widefat">
-                  <thead>
-                  <tr class="form-invalid">
-                      <th class="row-title">' . esc_html__( 'Message Debug Mode', 'formscrm' ) . '</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                  <td><pre>';
-			echo esc_html( $message );
-			echo '</pre></td></tr></table>';
+			error_log( 'FORMSCRM: ' . esc_html__( 'Message Debug Mode', 'formscrm' ) . ' ' . esc_html( $message ) );
 		}
 	}
 }
@@ -41,18 +36,22 @@ if ( ! function_exists( 'formscrm_error_admin_message' ) ) {
 	 * @return void
 	 */
 	function formscrm_error_admin_message( $code, $message ) {
-		if ( wp_doing_ajax() ) {
-			error_log( 'API ERROR ' . esc_html( $code ) . ': ' . esc_html( $message ) );
-		} else {
-			echo '<div class="error">';
-			echo '<p><strong>API ERROR ' . esc_html( $code ) . ': </strong> ' . esc_html( $message ) . '</p>';
-			echo '</div>';
+		if ( true === WP_DEBUG ) {
+			error_log( 'FORMSCRM: API ERROR ' . esc_html( $code ) . ': ' . esc_html( $message ) );
 		}
 	}
 }
 
 // * Sends an email to administrator when it not creates the lead
 if ( ! function_exists( 'formscrm_debug_email_lead' ) ) {
+	/**
+	 * Sends error to admin
+	 *
+	 * @param string $crm   CRM.
+	 * @param string $error Error to send.
+	 * @param array  $data  Data of error.
+	 * @return void
+	 */
 	function formscrm_debug_email_lead( $crm, $error, $data ) {
 		$to      = get_option( 'admin_email' );
 		$subject = 'FormsCRM - ' . __( 'Error creating the Lead', 'formscrm' );
@@ -68,10 +67,15 @@ if ( ! function_exists( 'formscrm_debug_email_lead' ) ) {
 }
 
 if ( ! function_exists( 'formscrm_testserver' ) ) {
+	/**
+	 * Error message
+	 *
+	 * @return void
+	 */
 	function formscrm_testserver() {
 		// test curl.
-		if ( ! function_exists( 'curl_version' ) ) {
-			echo '<div id="message" class="error below-h2"><p><strong>' . __( 'curl is not Installed in your server. It is needed to work with CRM Libraries.', 'formscrm' ) . '</strong></p></div>';
+		if ( ! function_exists( 'curl_version' ) && true === WP_DEBUG ) {
+			error_log( 'FORMSCRM: ' . __( 'curl is not Installed in your server. It is needed to work with CRM Libraries.', 'formscrm' ) );
 		}
 	}
 }
@@ -88,7 +92,7 @@ if ( ! function_exists( 'formscrm_check_url_crm' ) ) {
 		if ( ! isset( $url ) ) {
 			$url = '';
 		}
-		if ( substr( $url, -1 ) != '/' ) {
+		if ( substr( $url, -1 ) !== '/' ) {
 			$url .= '/'; // adds slash to url.
 		}
 
