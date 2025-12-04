da3bbada3ed0f61a7e11bde5b32792d3782ef8a0
castellon <alexcastellon077@gmail.com>
2025-12-04 13:13:03 +0100

    Fixes

--- includes/crm-library/class-crmlib-clientify.php
+++ includes/crm-library/class-crmlib-clientify.php
@@ -814,12 +814,12 @@ class CRMLIB_Clientify {
 					$deal_tags = $element['value'];
 				} elseif ( 'deal|expected_closed_date_days' === $element['name'] ) {
 					$deal['expected_closed_date'] = gmdate( 'Y-m-d', strtotime( '+' . (int) $element['value'] . ' days' ) );
-			} elseif ( 'deal|pipeline_name' === $element['name'] ) {
-				// Pipeline URL functionality not yet implemented.
-				// For now, use the pipeline name directly if provided.
-				if ( ! empty( $element['value'] ) ) {
-					$deal['pipeline'] = $element['value'];
-				}
+				} elseif ( 'deal|pipeline_name' === $element['name'] ) {
+					// Pipeline URL functionality not yet implemented.
+					// For now, use the pipeline name directly if provided.
+					if ( ! empty( $element['value'] ) ) {
+						$deal['pipeline'] = $element['value'];
+					}
 				} else {
 					$deal_field             = explode( '|', $element['name'] );
 					$deal[ $deal_field[1] ] = $element['value'];
