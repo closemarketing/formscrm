1bf27910e051aedda0cbbb56bec07cb37d2d0cbc
davidperezgar <david@closemarketing.es>
2025-12-04 08:19:00 +0100

    test

--- includes/formscrm-library/loader.php
+++ includes/formscrm-library/loader.php
@@ -72,7 +72,15 @@ if ( is_plugin_active( 'wpforms/wpforms.php' ) && ! class_exists( 'WPForms_Forms
 
 // Ninja Forms.
 if ( is_plugin_active( 'ninja-forms/ninja-forms.php' ) && ! class_exists( 'FormsCRM_NinjaForms_Action' ) ) {
-	require_once 'class-ninjaforms.php';
+	add_action(
+		'plugins_loaded',
+		function () {
+			if ( class_exists( 'NF_Abstracts_Action' ) ) {
+				require_once plugin_dir_path( __FILE__ ) . 'class-ninjaforms.php';
+			}
+		},
+		20
+	); // Priority 20 to load after Ninja Forms.
 }
 
 // Elementor.
